2e0fa6e402a5c5fa98edf99f6394cd6bc7b8f9b3
Abdulkarim Bashir termanini <87238276+Karim-Termanini@users.noreply.github.com>
2022-03-19 12:26:01 +0100

    Update Main.java

--- DownloadManager/src/Main.java
+++ DownloadManager/src/Main.java
@@ -2,9 +2,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		//DownloadsManager downloadsManager = new DownloadsManager("Test");
-		//https://filesamples.com/samples/document/txt/sample3.txt
-		
+		//der Link in dem downloadLink geben
 		String downloadLink = "https://download.samplelib.com/mp3/sample-15s.mp3";
 		new Thread(new DownloadsManager(downloadLink)).start();
 		
